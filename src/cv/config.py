0831cd1d6b7f6ecc2ddfce9752a0280613526618
Alex Thola <alexthola@gmail.com>
2023-07-19 01:10:18 -0500

    updating templates to put certs on sidebar so projects can be added to main body

--- src/cv/config.py
+++ src/cv/config.py
@@ -2,7 +2,7 @@
 """All modifiable constants."""
 
 # Paths
-PATH_DATA = "sample"
+PATH_DATA = "cv_private"
 # PATH_DATA = "sample"  # Uncomment if wanting to use the sample folder
 PATH_INPUT = f"{PATH_DATA}/input/"
 PATH_OUTPUT = f"{PATH_DATA}/output/"
